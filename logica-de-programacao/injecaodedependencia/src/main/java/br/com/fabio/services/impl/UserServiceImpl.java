5b1bcf9833a99657d0eb63401fe45977b0fc9aa2
fabiojdluz <fabio.jdluz@gmail.com>
2021-11-25 11:01:18 -0300

    COMMIT

--- logica-de-programacao/injecaodedependencia/src/main/java/br/com/fabio/services/impl/UserServiceImpl.java
+++ logica-de-programacao/injecaodedependencia/src/main/java/br/com/fabio/services/impl/UserServiceImpl.java
@@ -1,16 +1,15 @@
 package br.com.fabio.services.impl;
 
-import java.lang.annotation.*;
+import br.com.fabio.framework.annotations.Component;
+import br.com.fabio.services.UserService;
 
-/**
- *  Service field variables should use this annotation
- *  This annotation Can be used to avoid conflict if there are multiple implementations of the same interface
- */
-@Target({ ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER,
-	ElementType.TYPE, ElementType.ANNOTATION_TYPE })
-@Retention(RetentionPolicy.RUNTIME)
-@Inherited
-@Documented
-public @interface UserServiceImpl {
-	String value() default "";
+
+
+@Component
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public String getUserName() {
+		return "username";
+	}
 }
