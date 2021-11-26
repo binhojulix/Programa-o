eeedafa832d238e2cbf2c1bb76677a6f78ea0bbf
fabiojdluz <fabio.jdluz@gmail.com>
2021-11-26 15:20:03 -0300

    commit

--- logica-de-programacao/logicadeprogramacao/src/main/java/br/com/fabio/App.java
+++ logica-de-programacao/logicadeprogramacao/src/main/java/br/com/fabio/App.java
@@ -9,9 +9,49 @@ public final class App {
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Produto produtos[] = new Produto[5];
+        produtos[0] = new Produto("Lamborghini", 1000000);
+        produtos[1] = new Produto("Jipe", 46000);
+        produtos[2] = new Produto("Brasília", 16000);
+        produtos[3] = new Produto("Smart", 46000);
+        produtos[4] = new Produto("Fusca", 17000);
+
+        // int maisBarato = buscaMenor(produtos);
+        int maisBarato = buscaMenor(produtos, 0, 4);
+        for (int atual = 0; atual <= 4; atual++) {
+            if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
+                maisBarato = atual;
+            }
+        }
+        System.out.println(maisBarato);
+        System.out.println("O	carro	mais	barato	custa "
+                + produtos[maisBarato].getPreco());
+
     }
+
+    private static int buscaMenor(Produto[] produtos) {
+        int maisBarato = 0;
+        for (int atual = 0; atual <= 4; atual++) {
+            if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
+                maisBarato = atual;
+            }
+        }
+        return maisBarato;
+    }
+
+    private static int buscaMenor(Produto[] produtos, int inicio, int termino) {
+        int maisBarato = inicio;
+        for (int atual = inicio; atual <= termino; atual++) {
+            if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
+                maisBarato = atual;
+            }
+        }
+        return maisBarato;
+    }
+
 }
---
+++ logica-de-programacao/logicadeprogramacao/src/main/java/br/com/fabio/Produto.java
@@ -0,0 +1,26 @@
+package br.com.fabio;
+
+
+public class Produto {
+    
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public double getPreco(){
+        return this.preco;
+    }
+    public void setPreco(double preco){
+        this.preco = preco;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+}
